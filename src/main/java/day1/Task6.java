5ecfe0afaf4cf7644f1acc7e88b775ac57c52ac3
Arkady Yuzhakov <arcadiy.yuzhakov@gmail.com>
2020-11-30 12:25:05 +0500

    Add Day1 task solutions

--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -2,6 +2,11 @@ package day1;
 
 public class Task6 {
     public static void main(String[] args) {
-
+        int k = 7;
+        int counter = 1;
+        while(counter <= 10) {
+            System.out.println(counter + " x " + k + " = " + (counter*k));
+            counter++;
+        }
     }
 }
