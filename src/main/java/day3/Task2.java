466970d974e00adb2cae5011bd34b65b5f641b3c
Arkady Yuzhakov <arcadiy.yuzhakov@gmail.com>
2020-12-02 18:00:54 +0500

    Day 3 Tasks solutions add

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,23 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        double value1;
+        double value2;
+        double wrongValue2 = 0;
+        double result;
+
+        while (wrongValue2 == 0) {
+            value1 = input.nextDouble();
+            value2 = input.nextDouble();
+            result = value1 / value2;
+            if (value2 == wrongValue2) {
+                break;
+            } else System.out.println(result);
+        }
     }
 }
--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,26 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+
+        double result;
+        double wrongValue2 = 0;
+
+        for (int i = 0; i < 5; i++) {
+            double value1 = input.nextDouble();
+            double value2 = input.nextDouble();
+
+            if (value2 == wrongValue2) {
+                System.out.println("Деление на 0");
+                continue;
+            }
+
+            result = value1 / value2;
+            System.out.println(result);
+        }
     }
 }
