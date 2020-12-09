31d00154f7670673b962b0b55dd00960041b80c8
Arkady Yuzhakov <arcadiy.yuzhakov@gmail.com>
2020-12-09 18:17:00 +0500

    Day 8 Tasks solutions Add

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,21 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        String string1 = "";
 
+        for (int i = 0; i <= 20000; i++) {
+            string1 += i + " ";
+        }
+        System.out.println(string1);
+        System.out.println("Строка оставлена конкатенацией за " + System.nanoTime());
+
+
+        StringBuilder string2 = new StringBuilder();
+
+        for (int i = 0; i <= 20000; i++) {
+            string2.append(i + " ");
+        }
+        System.out.println(string2);
+        System.out.println("Строка оставлена через StringBuilder за " + System.nanoTime());
     }
 }
